709d67627b1f6cb7124f916a6dee6f5086c62ec9
Tamirys Hariadny Camargo Couto <tamirys.couto@escola.pr.gov.br>
2026-02-25 13:04:32 +0000

    ai

--- calculadoraIMC.js
+++ calculadoraIMC.js
@@ -1,20 +1,20 @@
-function calculoIMC(peso, altura){
+function calculoIMC(peso, altura) {
     let imc = peso / (altura ** 2);
 
     console.log("Seu IMC é, " + imc);
 
-    if(imc < 18.5){
+    if (imc < 18.5) {
         console.log("Classificação: Abaixo do peso");
     }
-    else if(imc >= 18.5 && imc <= 24.9){
+    else if (imc >= 18.5 && imc <= 24.9) {
         console.log("Classificação: Peso normal");
     }
-    else if(imc >= 25 && imc <= 29.9){
+    else if (imc >= 25 && imc <= 29.9) {
         console.log("Classificação: Sobrepeso");
     }
-else { 
-    console.log("Classificação: Obesidade");
-}
+    else {
+        console.log("Classificação: Obesidade");
+    }
 }
 calculoIMC(70, 1.63)
 
